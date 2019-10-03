ccd0266bdfe886a93cf249674332265b9630a69d
Akhil Chhetri <achettri.mbsys@gmail.com>
2019-10-03 16:09:43 +0545

    Basic CRUD app using .net core and C#

---
+++ Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCrud.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        
+        [Column(TypeName ="varchar(250)")]
+        [Required]
+        [System.ComponentModel.DisplayName("Full Name")]
+        public string FullName { get; set; }
+
+        [Column (TypeName ="varchar(10)")]
+        [System.ComponentModel.DisplayName("Emp Code")]
+        public string EmpCode { get; set; }
+        
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+        [System.ComponentModel.DisplayName("Office Location")]
+        [Column(TypeName = "varchar(100)")]
+
+        public string OfficeLocation { get; set; }
+
+    }
+}
---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCrud.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options):base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
